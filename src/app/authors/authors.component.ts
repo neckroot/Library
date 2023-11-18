b5d4e026b6c17e13007176b02dd72647d669a4d6
neckroot <rfonl1ne@yandex.ru>
2023-11-18 21:45:31 +0300

    add reusable form component

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
 import { TableComponent } from '../table/table.component';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-authors',
@@ -10,4 +11,30 @@ import { TableComponent } from '../table/table.component';
   imports: [InputComponent, TableComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthorsComponent {}
+export class AuthorsComponent {
+  public fields = [
+    {
+      field: 'lastname',
+      name: 'Фамилия',
+    },
+    {
+      field: 'firstname',
+      name: 'Имя',
+    },
+    {
+      field: 'patronymic',
+      name: 'Отчество',
+    },
+    {
+      field: 'birthdate',
+      name: 'Дата рождения',
+    },
+  ];
+
+  public profileForm = new FormGroup({
+    lastname: new FormControl(''),
+    firstname: new FormControl(''),
+    patronymic: new FormControl(''),
+    birthdate: new FormControl(''),
+  });
+}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
 import { TableComponent } from '../table/table.component';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-books',
@@ -8,6 +15,38 @@ import { TableComponent } from '../table/table.component';
   styleUrl: './books.component.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [InputComponent, TableComponent],
+  imports: [
+    InputComponent,
+    TableComponent,
+    FormsModule,
+    NgFor,
+    ReactiveFormsModule,
+  ],
 })
-export default class BooksComponent {}
+export default class BooksComponent {
+  public fields = [
+    {
+      field: 'author',
+      name: 'Автор',
+    },
+    {
+      field: 'title',
+      name: 'Название книги',
+    },
+    {
+      field: 'publisher',
+      name: 'Издатель',
+    },
+    {
+      field: 'year',
+      name: 'Год издания',
+    },
+  ];
+
+  public profileForm = new FormGroup({
+    author: new FormControl(''),
+    title: new FormControl(''),
+    publisher: new FormControl(''),
+    year: new FormControl(''),
+  });
+}
